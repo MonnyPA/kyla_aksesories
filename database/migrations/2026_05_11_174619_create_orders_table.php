e5d5054677ef4a950f186fe8141c22f567734a74
BM <monnybram@outlook.com>
2026-05-11 18:01:10 +0900

    Migrate Done

---
+++ database/migrations/2026_05_11_174619_create_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_code')->unique();
+            $table->unsignedBigInteger('user_id');
+            $table->integer('total');
+            $table->enum('status_pembayaran', ['pending','settlement']);
+            $table->enum('payment_method', ['tunai','qris']);
+            $table->text('note')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
