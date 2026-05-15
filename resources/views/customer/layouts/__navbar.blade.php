f5fe7f34bdeedb6445536df81abeef641421179e
BM <monnybram@outlook.com>
2026-05-15 17:30:19 +0900

    Edit Name Product to Penjualan Product

--- resources/views/customer/layouts/__navbar.blade.php
+++ resources/views/customer/layouts/__navbar.blade.php
@@ -8,7 +8,7 @@
                     <div class="collapse navbar-collapse bg-white" id="navbarCollapse">
                         <div class="navbar-nav mx-auto">
                             <a href="#" class="nav-item nav-link">Dashboard</a>
-                            <a href="{{ route('listproduct') }}" class="nav-item nav-link {{ request()->routeIs('listproduct') ? 'active' : '' }}">Product</a>
+                            <a href="{{ route('listproduct') }}" class="nav-item nav-link {{ request()->routeIs('listproduct') ? 'active' : '' }}">Penjualan Product</a>
                             <a href="{{ route('orders.index') }}" class="nav-item nav-link {{ request()->routeIs('orders.*') ? 'active' : '' }}">Daftar Pesanan</a>
                             <a href="{{ route('products.index') }}" class="nav-item nav-link {{ request()->routeIs('products.*') ? 'active' : '' }}">Kelola Product</a>
                             <a href="{{ route('categories.index') }}" class="nav-item nav-link {{ request()->routeIs('categories.*') ? 'active' : '' }}">Kelola Category</a>
